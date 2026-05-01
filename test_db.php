ef8fc867c9e2e871f649beede24f56a06de9ef3c
Gabrieldc12 <lordgabrielc@gmail.com>
2026-05-01 18:34:07 +0800

    cleaning

--- test_db.php
+++
@@ -1,5 +0,0 @@
-<?php
-require 'db_connection.php';
-$res = $conn->query("SELECT id, scheduled_hours FROM daily_attendance WHERE attendance_date='2026-04-12'");
-while($r = $res->fetch_assoc()) var_dump($r);
-?>
